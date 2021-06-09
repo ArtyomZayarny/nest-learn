cc45b90004cabc123cdc8678a8f17102c7b3c112
ArtyomZayarny <artyom.zayarny@gmail.com>
2021-06-09 07:56:31 +0300

    created task with repository

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task-dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter-dto';
 import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
+import {Task} from './task.entity'; 
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
@@ -17,16 +18,16 @@ export class TasksController {
     //     }
     // }
 
-    // @Get('/:id')
-    // getTaskById(@Param('id') id:string) {
-    //     return this.tasksService.getTaskById(id);
-    // }
+    @Get('/:id')
+    getTaskById(@Param('id', ParseIntPipe) id:number):Promise<Task> {
+        return this.tasksService.getTaskById(id);
+    }
 
-    // @Post()
-    // @UsePipes(ValidationPipe)
-    // createTask(@Body() createTaskDto: CreateTaskDto): Task {
-    //   return this.tasksService.createTask(createTaskDto);
-    // }
+    @Post()
+    @UsePipes(ValidationPipe)
+    createTask(@Body() createTaskDto: CreateTaskDto): Promise<Task>{
+      return this.tasksService.createTask(createTaskDto);
+    }
 
     // @Delete('/:id')
     // deleteTask(@Param('id') id:string) {
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,9 +1,19 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import {Task} from './task.entity';
 import { CreateTaskDto } from './dto/create-task-dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter-dto';
+import { TaskRepository } from './task.repository';
+import { TaskStatus } from './task-status.enum';
 
 @Injectable()
 export class TasksService {
+    constructor (
+        @InjectRepository(TaskRepository)
+        private taskRepository:TaskRepository
+    ) {
+        
+    }
    
     // getAllTasks():Task[] {
     //     return this.tasks;
@@ -28,6 +38,14 @@ export class TasksService {
     //     return tasks
     // }
 
+    async getTaskById(id:number):Promise<Task> {
+        const found = await this.taskRepository.findOne(id);
+     if (!found) {
+            throw new NotFoundException(`Task with ID: ${id} not found `);
+        }
+      return found
+    }
+
     // getTaskById(id:string):Task {
     //     const found = this.tasks.find( task => task.id === id);
 
@@ -42,6 +60,10 @@ export class TasksService {
     //     const found = this.getTaskById(id);  
     //     this.tasks = this.tasks.filter(task => task.id !== id);
     // }
+
+   async createTask(createTaskDto: CreateTaskDto) {
+       return this.taskRepository.createTask(createTaskDto)
+    }
 
     // createTask(createTaskDto: CreateTaskDto): Task {
     //     const { title, description } = createTaskDto;
