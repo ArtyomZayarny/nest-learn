222d06c744c260b333c6008184494d490fda7b5a
ArtyomZayarny <artyom.zayarny@gmail.com>
2021-06-09 08:23:04 +0300

    delete task with repository

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -56,26 +56,17 @@ export class TasksService {
     //     return found
     // }
 
-    // deleteTask(id: string):void {
-    //     const found = this.getTaskById(id);  
-    //     this.tasks = this.tasks.filter(task => task.id !== id);
-    // }
+   async deleteTask(id:number):Promise<void> {
+        const result = await this.taskRepository.delete(id)
+
+        if (result.affected === 0) {
+           throw new NotFoundException(`Task with ID: ${id} not found `);
+        }
+    }
 
    async createTask(createTaskDto: CreateTaskDto) {
        return this.taskRepository.createTask(createTaskDto)
     }
-
-    // createTask(createTaskDto: CreateTaskDto): Task {
-    //     const { title, description } = createTaskDto;
-    //     const task: Task = {
-    //         id: uuidv4(),
-    //         title,
-    //         description,
-    //         status: TaskStatus.OPEN
-    //     }
-    //     this.tasks.push(task);
-    //     return task;
-    // }
 
     // updateTaskStatus(id: string, status: TaskStatus):Task {
     //     const task = this.getTaskById(id);
